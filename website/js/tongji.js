1739809f0514ff972da0b58a8973fd5c642ace94
VegaZgame <yongkang@chinawebeye.com>
2020-04-08 10:09:24 +0800

    update 统计代码

---
+++ website/js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ca99a8cb74e540c329dba3bd85ab2b93";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
